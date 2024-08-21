c579dfd0ae46a2e05e7280118402e7370d9d1534
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-08-21 17:51:33 +0530

    array2commited

--- Array2.java
+++ Array2.java
@@ -1,3 +1,19 @@
 public class Array2{
   // Prepare an Array with values of 1,2,3,4,5 and replace the value at 3rd index with 45
+  public static void main(String[] args) {
+    int[] k = new int[5];
+    for (int i = 0; i < k.length; i++) {
+        k[i] = i+1;
+    }
+    System.out.println("arrays values");
+    for (int i = 0;i<k.length;i++) {
+        if(i==2){
+          k[i]=45;
+    }
+}
+ for(int a:k){
+   System.out.println(k);
+ }
+}
+
 }
